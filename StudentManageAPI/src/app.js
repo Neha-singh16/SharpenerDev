3fe7b1a075c28dd70aa8b739cf54d70d959512c7
Neha-singh18 <singhrajputneha896@gmail.com>
2026-06-30 16:55:36 +0530

    Build a Student Management API

---
+++ StudentManageAPI/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const db = require("../utils/db-connection");
+const studentRouter = require("../router/studentRouter");
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("Hieeeee!!");
+})
+
+app.use("/students", studentRouter);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
