fd43189c7d2fc8ae518b4ce855a8410d39c5c675
Neha-singh18 <singhrajputneha896@gmail.com>
2026-07-19 20:05:44 +0530

    done the optimizations and  maintaining a precomputed

--- expenseTracker/backend/services/premiumServices.js
+++ expenseTracker/backend/services/premiumServices.js
@@ -3,46 +3,57 @@ const Expense = require("../models/expenseModel");
 const seq = require("../utils/db");
 
 
-const getLeaderboard = async (req, res) => {
-  // const leaderBoard = await User.findAll({
-  //   attributes: [
-  //     "id",
-  //     "name",
-  //     "email",
-  //     [
-  //       seq.fn(
-  //         "SUM",
-  //         seq.col("expenses.amount")
-  //       ),
-  //       "totalExpense"
-  //     ]
-  //   ],
-  //   include: [
-  //     {
-  //       model: Expense,
-  //       attributes: []
-  //     }
-  //   ],
-  //   group: ["User.id"],
-  //   order: [[seq.literal("totalExpense"), "DESC"]]
+// const getLeaderboard = async (req, res) => {
+//   // const leaderBoard = await User.findAll({
+//   //   attributes: [
+//   //     "id",
+//   //     "name",
+//   //     "email",
+//   //     [
+//   //       seq.fn(
+//   //         "SUM",
+//   //         seq.col("expenses.amount")
+//   //       ),
+//   //       "totalExpense"
+//   //     ]
+//   //   ],
+//   //   include: [
+//   //     {
+//   //       model: Expense,
+//   //       attributes: []
+//   //     }
+//   //   ],
+//   //   group: ["User.id"],
+//   //   order: [[seq.literal("totalExpense"), "DESC"]]
 
-  // });
+//   // });
 
-  // return leaderBoard;
+//   // return leaderBoard;
 
-  const leaderBoard = await User.findAll({
-    // sort by totalExpense in desc
-     attributes: ["id", "name", "totalExpense"],
-  order: [["totalExpense", "DESC"]],
+//   const getLeaderboard = await User.findAll({
+//     // sort by totalExpense in desc
+//      attributes: ["id", "name", "totalExpense"],
+//   order: [["totalExpense", "DESC"]],
   
-  })
+//   })
 
 
-return res.status(200).json({
-    leaderBoard
-});
+// return res.status(200).json({
+//     leaderBoard: getLeaderboard
+// });
 
+// };
+
+const getLeaderboard = async () => {
+
+    const leaderBoard = await User.findAll({
+        attributes: ["id", "name", "totalExpense"],
+        order: [["totalExpense", "DESC"]],
+    });
+
+    return leaderBoard;
 };
 
+module.exports = { getLeaderboard };
 
-module.exports = { getLeaderboard};
+
