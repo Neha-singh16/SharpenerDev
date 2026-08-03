a67a91c9f9c48c0ac55f91781985596d608030ae
Neha-singh18 <singhrajputneha896@gmail.com>
2026-08-03 18:59:56 +0530

    Make the Messages Live (Polling & WebSockets)

--- chatBotApp/backend/src/app.js
+++ chatBotApp/backend/src/app.js
@@ -1,28 +1,43 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 const db = require("./database/db-connection");
 const userRouter = require("./routers/userRouter");
 const chatRouter = require("./routers/chatRouter");
- require("./models/index");
-
+require("./models/index");
 
 app.use(express.json());
 app.use(cors());
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+app.set("io", io);
+io.on("connection", (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`Socket disconnected: ${socket.id}`);
+  });
+});
 app.use("/users", userRouter);
 app.use("/chat", chatRouter);
 
 const PORT = process.env.PORT || 3000;
 
 db.sync()
-.then(() => {
+  .then(() => {
     console.log("Database synced successfully.");
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
--- chatBotApp/backend/src/controllers/chatController.js
+++ chatBotApp/backend/src/controllers/chatController.js
@@ -1,9 +1,12 @@
 const chatService = require("../services/chatService");
 
+
 async function postMessage(req, res) {
   try {
+    const io = req.app.get("io");
     const { message } = req.body;
     const chat = await chatService.postMessage(req.user.id, message);
+    io.emit("receive-message", chat);
     res.status(201).json({ message: "Message posted successfully", chat });
   } catch (err) {
     res.status(500).json({
@@ -15,7 +18,9 @@ async function postMessage(req, res) {
 async function getAllMessages(req,res){
     try{
         const chats = await chatService.getAllMessages();
+        
           res.status(201).json({ message: "Message fetched successfully", chats });
+         
 
     }catch(err){
          res.status(500).json({
@@ -23,7 +28,6 @@ async function getAllMessages(req,res){
     });
     }
 }
-
 
 
 module.exports = {
